a85f27cd517d130d3a4f85044ef6f6c39ac0e5b4
Maciek Kostrzycki <m.kostrzycki@global4net.com>
2019-07-19 14:26:07 +0200

    Little refactor.

--- src/Model/Resolver/CmsPage.php
+++ src/Model/Resolver/CmsPage.php
@@ -4,19 +4,13 @@ namespace G4NReact\MsCatalogMagento2GraphQl\Model\Resolver;
 
 use G4NReact\MsCatalog\Client\ClientFactory;
 use G4NReact\MsCatalog\ResponseInterface;
-use G4NReact\MsCatalogMagento2\Helper\Query;
-use G4NReact\MsCatalogMagento2\Helper\Cms\CmsQuery;
-use G4NReact\MsCatalogMagento2\Helper\Config as ConfigHelper;
-use Magento\Framework\App\CacheInterface;
-use Magento\Framework\App\DeploymentConfig;
-use Magento\Framework\Event\Manager as EventManager;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use G4NReact\MsCatalogMagento2\Helper\Cms\Field as HelperCmsField;
-use Magento\Framework\Serialize\Serializer\Json;
-use Magento\Store\Model\StoreManagerInterface;
-use Psr\Log\LoggerInterface;
 
 /**
  * Class CmsPage
@@ -26,13 +20,14 @@ class CmsPage extends AbstractResolver
 {
     /**
      * @param Field $field
-     * @param \Magento\Framework\GraphQl\Query\Resolver\ContextInterface $context
+     * @param ContextInterface $context
      * @param ResolveInfo $info
      * @param array|null $value
      * @param array|null $args
-     *
-     * @return array|\Magento\Framework\GraphQl\Query\Resolver\Value|mixed
+     * @return array
      * @throws GraphQlInputException
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
      */
     public function resolve(
         Field $field,
@@ -40,11 +35,11 @@ class CmsPage extends AbstractResolver
         ResolveInfo $info,
         array $value = null,
         array $args = null
-    )
-    {
+    ) {
         if (!isset($args['id'])) {
             throw new GraphQlInputException(__('id for cms page should be specified'));
         }
+
         return $this->getCmsPageFromSearchEngine($args['id'], $info->getFieldSelection(0));
     }
 
@@ -52,10 +47,9 @@ class CmsPage extends AbstractResolver
     /**
      * @param int $id
      * @param array $queryFields
-     *
      * @return array
-     * @throws \Magento\Framework\Exception\LocalizedException
-     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
      */
     public function getCmsPageFromSearchEngine(int $id, $queryFields = [])
     {
