3c594df4a4991a477c7d8c59168b790da81daa15
norbert <n.malecki@global4net.com>
2019-10-01 16:16:31 +0200

    handle category_id in attribute resolver

--- src/Model/Resolver/DataProvider/AttributeOption.php
+++ src/Model/Resolver/DataProvider/AttributeOption.php
@@ -8,6 +8,9 @@ use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Serialize\Serializer\Json;
 use Magento\Store\Model\StoreManagerInterface;
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory as CategoryCollectionFactory;
+use Magento\Catalog\Model\ResourceModel\Category\Collection as CategoryColleciton;
+use Magento\Catalog\Model\Category;
 
 /**
  * Class AttributeOption
@@ -15,80 +18,126 @@ use Magento\Store\Model\StoreManagerInterface;
  */
 class AttributeOption
 {
-    /**
-     * Attribute options cache key
-     */
-    const CACHE_KEY = 'G4N_MS-CAT-ATTR-OPT_CACHE';
+	/**
+	 * Attribute options cache key
+	 */
+	const CACHE_KEY = 'G4N_MS-CAT-ATTR-OPT_CACHE';
 
-    /**
-     * @var EavConfig
-     */
-    protected $eavConfig;
+	/**
+	 * Category id code
+	 */
+	const CATEGORY_ID = 'category_id';
 
-    /**
-     * @var StoreManagerInterface
-     */
-    protected $storeManager;
+	/**
+	 * @var EavConfig
+	 */
+	protected $eavConfig;
 
-    /**
-     * @var CacheInterface
-     */
-    protected $cache;
+	/**
+	 * @var StoreManagerInterface
+	 */
+	protected $storeManager;
 
-    /**
-     * @var Json
-     */
-    protected $serializer;
+	/**
+	 * @var CacheInterface
+	 */
+	protected $cache;
 
-    /**
-     * AttributeOption constructor
-     *
-     * @param EavConfig $eavConfig
-     * @param StoreManagerInterface $storeManager
-     * @param CacheInterface $cache
-     * @param Json $serializer
-     */
-    public function __construct(
-        EavConfig $eavConfig,
-        StoreManagerInterface $storeManager,
-        CacheInterface $cache,
-        Json $serializer
-    ) {
-        $this->eavConfig = $eavConfig;
-        $this->storeManager = $storeManager;
-        $this->cache = $cache;
-        $this->serializer = $serializer;
-    }
+	/**
+	 * @var Json
+	 */
+	protected $serializer;
 
-    /**
-     * @param $value
-     * @return array
-     * @throws NoSuchEntityException
-     * @throws LocalizedException
-     */
-    public function getData(array $value)
-    {
-        if (isset($value['attribute_options']) && !empty($value['attribute_options'])) {
-            return $value['attribute_options'];
-        }
+	/**
+	 * @var CategoryCollectionFactory
+	 */
+	protected $categoryCollectionFactory;
 
-        $attributeCode = $value['attribute_code'];
-        $cacheIdentifier = self::CACHE_KEY . ':' . $attributeCode . ':' . $this->storeManager->getStore()->getId();
+	/**
+	 * AttributeOption constructor
+	 *
+	 * @param EavConfig $eavConfig
+	 * @param StoreManagerInterface $storeManager
+	 * @param CacheInterface $cache
+	 * @param Json $serializer
+	 */
+	public function __construct(
+		EavConfig $eavConfig,
+		StoreManagerInterface $storeManager,
+		CacheInterface $cache,
+		Json $serializer,
+		CategoryCollectionFactory $categoryCollectionFactory
 
-        if ($cached = $this->cache->load($cacheIdentifier)) {
-            $attributeOptions = $this->serializer->unserialize($cached);
-            if ($attributeOptions) {
-                return $attributeOptions;
-            }
-        }
+	) {
+		$this->eavConfig = $eavConfig;
+		$this->storeManager = $storeManager;
+		$this->cache = $cache;
+		$this->serializer = $serializer;
+		$this->categoryCollectionFactory = $categoryCollectionFactory;
+	}
 
-        $attribute = $this->eavConfig->getAttribute('catalog_product', $attributeCode);
-        $attributeOptions = $attribute->getSource()->getAllOptions();
+	/**
+	 * @param $value
+	 * @return array
+	 * @throws NoSuchEntityException
+	 * @throws LocalizedException
+	 */
+	public function getData(array $value)
+	{
+		if (isset($value['attribute_options']) && !empty($value['attribute_options'])) {
+			return $value['attribute_options'];
+		}
 
-        if (is_array($attributeOptions)) {
-            $this->cache->save($this->serializer->serialize($attributeOptions), $cacheIdentifier, [], 86400);
-        }
+		$attributeCode = $value['attribute_code'];
+		$cacheIdentifier = self::CACHE_KEY . ':' . $attributeCode . ':' . $this->storeManager->getStore()->getId();
 
-        return $attributeOptions;
-    }
+		if ($cached = $this->cache->load($cacheIdentifier)) {
+			$attributeOptions = $this->serializer->unserialize($cached);
+			if ($attributeOptions) {
+				return $attributeOptions;
+			}
+		}
+
+		if ($attributeCode != self::CATEGORY_ID) {
+			$attribute        = $this->eavConfig->getAttribute('catalog_product', $attributeCode);
+			$attributeOptions = $attribute->getSource()->getAllOptions();
+		} else {
+			$attributeOptions = $this->getCategories();
+		}
+
+		if (is_array($attributeOptions)) {
+			$this->cache->save($this->serializer->serialize($attributeOptions), $cacheIdentifier, [], 86400);
+		}
+
+		return $attributeOptions;
+	}
+
+	protected function getCategories()
+	{
+		/** @var CategoryColleciton $collection */
+		$collection = $this->categoryCollectionFactory->create();
+		$collection
+			->addFieldToSelect('name')
+			->setStore($this->getStoreId());
+
+		$categoriesArray = [];
+		/** @var Category $category */
+		foreach ($collection as $category){
+			$categoriesArray[] = [
+				'value' => $category->getId(),
+				'label' => $category->getName()
+			];
+		}
+
+		return $categoriesArray;
+	}
+
+	/**
+	 * @return int
+	 * @throws NoSuchEntityException
+	 */
+	protected function getStoreId() : int
+	{
+		return $this->storeManager->getStore()->getId();
+	}
 }
